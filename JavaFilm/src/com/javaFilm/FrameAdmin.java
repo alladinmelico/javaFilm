206e854bc333c2c03789f55f4b04c178c3be1abb
alladinMelico <melico.alladin@gmail.com>
2019-11-17 15:25:33 +0800

    created main frame

--- JavaFilm/src/com/javaFilm/FrameAdmin.java
+++ JavaFilm/src/com/javaFilm/FrameAdmin.java
@@ -1,13 +1,16 @@
 package com.javaFilm;
 
 import javax.swing.JFrame;
+import java.awt.Dimension;
 
 public class FrameAdmin  extends JFrame {
 	public FrameAdmin() {
+		setResizable(false);
+		setSize(new Dimension(1024, 768));
+		
 		setVisible(true);
 		setTitle("Pine Valley Furniture");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(false);
 	}
 
 }
